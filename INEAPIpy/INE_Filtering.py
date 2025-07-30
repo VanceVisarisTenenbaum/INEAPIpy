78b02cbbee7b909eb71991ee08d3f98660fb4561
Eli <144833048+VanceVisarisTenenbaum@users.noreply.github.com>
2025-07-30 17:28:33 +0100

    page is None check fix

--- INEAPIpy/INE_Filtering.py
+++ INEAPIpy/INE_Filtering.py
@@ -131,8 +131,10 @@ def date_count_selection_params_builder(list_of_dates: list | None = None,
         DESCRIPTION.
 
     """
-    if list_of_dates is None and count is None:
-        raise ValueError('At least one must be provided, count or dates.')
+    if list_of_dates is None and count is None and page is None:
+        raise ValueError(
+            'At least one must be provided, count or dates or page.'
+        )
     # pydantic checks. This raises error if input isn't correctly shaped.
     Inputs = FIM.FilteringInputs(
         list_of_dates=list_of_dates, count=count,
--- check_everything_is_correct.py
+++ check_everything_is_correct.py
@@ -2,5 +2,13 @@
 
 import INEAPIpy.Bridge as Bridge
 
-INE = Bridge.EasyINEAPIClientSync()
+INE = Bridge.EasyINEAPIClientSync(mode='pydantic')
 
+
+"""
+This file is here just to run every possible option and check everything is
+working as intended.
+"""
+
+
+INE.get_operations_()
